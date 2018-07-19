15ba068e012f3df8268cf304b90d1c19255bcefb
JovenSR <jovensrai@gmail.com>
2018-07-19 17:23:20 +0000

    shows empty cart message and links back to home page if cart is empty

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
+  # before_filter :authorize
+
   http_basic_authenticate_with name: ENV['BASIC_USER'], password: ENV['BASIC_PASSWORD']
   def show
   end
